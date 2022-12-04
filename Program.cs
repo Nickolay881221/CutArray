fc2f01004bda798f51501b1d733984ad45434c41
Nick Volodin <Volod-nick@mail.ru>
2022-12-04 13:05:53 +0300

    Cut Array function done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которык меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] TextMassive = {"hello", "2", "world", ":-)"};
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+string[] CutArray (string[] firstarray)
+{
+    int m = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+        if (firstarray[i].Length <= 3)
+        {
+            m++;
+        }
+    }
+    
+    string[] newarray = new string[m];
+    int count = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+        if (firstarray[i].Length <= 3)
+        {
+            newarray[count] = firstarray[i];
+            count++;
+        }
+    }
+    return newarray;
+}
+
+PrintArray(TextMassive);
+System.Console.WriteLine();
+string[] finalarray = CutArray(TextMassive);
+PrintArray(finalarray);
